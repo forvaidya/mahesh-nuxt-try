fdaaab1404905bd8874d2e22a8c2ebf2ca46f874
MAHESH VAIDYA <forvaidya@gmail.com>
2025-03-09 18:30:40 +0530

    fix

--- plugins/usercentrics.js
+++ plugins/usercentrics.js
@@ -1,6 +1,8 @@
 export default ({ app }) => {
     if (process.client) {
       (function (w, d, s, o, f, js, fjs) {
+        console.log('Usercentrics script Begin');
+
         w['Usercentrics'] = o;
         w[o] = w[o] || function () {
           (w[o].q = w[o].q || []).push(arguments);
@@ -11,10 +13,11 @@ export default ({ app }) => {
         js.src = f;
         js.async = 1;
         fjs.parentNode.insertBefore(js, fjs);
+        console.log('Usercentrics script loaded');
       })(window, document, 'script', 'Usercentrics', 'https://app.usercentrics.eu/browser-ui/latest/loader.js');
   
       Usercentrics('init', {
-        settingsId: 'YOUR_SETTINGS_ID',
+        settingsId: 'T6i2FL-oQnGM5b',
         version: 1,
         language: 'en',
         // Additional configurations can be added here
