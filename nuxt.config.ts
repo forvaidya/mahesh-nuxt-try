b2cf754578388503bb5c8f9fd5092e7cb67439a1
Mahesh Vaidya <forvaidya@gmail.com>
2025-03-09 19:36:05 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   plugins: [
-   '~/plugins/usercentrics.js'
+  // '~/plugins/usercentrics.js'
   ],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false }
