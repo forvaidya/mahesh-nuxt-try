c3f6e0c5d009bc69d9238af0e062473ebe7ea03f
MAHESH VAIDYA <forvaidya@gmail.com>
2025-03-09 18:17:51 +0530

    plugins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  plugins: [
+    '~/plugins/usercentrics.js'
+  ],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false }
 })
+
